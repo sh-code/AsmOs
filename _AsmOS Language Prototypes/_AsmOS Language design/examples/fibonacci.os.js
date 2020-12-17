00a4809e6b2a447d58339e7f8d3dc0824fb050d8
Miroslav Martinovič - sh code <midnight.st@gmail.com>
2020-12-17 23:42:33 +0100

    updated fibonacci and include example. builtin keywords will be short abbreviations, actual logic-relevant variables need to be proper descriptive, so let's take up all the 3-4 letter abbrvs with keywords

--- _AsmOS Language Prototypes/_AsmOS Language design/examples/fibonacci.os.js
+++ _AsmOS Language Prototypes/_AsmOS Language design/examples/fibonacci.os.js
@@ -1,10 +1,10 @@
 /* up-to-date-for: 2020-12-17 */
 
 //shortest and simplest possible example of fibonacci full program
-public fibonacci { //object/namespace/block name. when marked as public, when this program's file is included by another program (from different file), it can be accessed as a library
-    public int[] iterative(int length){
+pub fibonacci { //object/namespace/block name. when marked as public, when this program's file is included by another program (from different file), it can be accessed as a library
+    pub int[] iterative(int length){
         int[length] res = [1, 1];   //seed with initial two values. //could also be res = [0:1, 1:1] with explicitly specified indexes. //that way i can seed arbitrary positions like res = [25:1, 30:1];
-        throuhg(position => [2 .. length]){
+        thr(position => [2 .. length]){
             res[position] = res[position - 1] + res[position - 2];
         }
 
@@ -12,7 +12,7 @@ public fibonacci { //object/namespace/block name. when marked as public, when th
     }
 
 
-    public int[] recursive(int position, int length, int[] sequence = null){
+    pub int[] recursive(int position, int length, int[] sequence = null){
         if(position < 2) return recursive(2, length, new int[2]{1, 1});
         else if(position < length) 
             return recursive(position + 1, length, sequence.append(sequence[position-1] + sequence[position-2]));
--- _AsmOS Language Prototypes/_AsmOS Language design/examples/include.os.js
+++ _AsmOS Language Prototypes/_AsmOS Language design/examples/include.os.js
@@ -1,8 +1,8 @@
 /* up-to-date-for: 2020-12-17 */
 
 main {
-    include fibonacciProgram = "fibonacci.os.js"; //assign the include into a variable if we want to be able to run the whole program
-    include "fibonacci.os.js"; //ignore the main program, just include the public namespaces from the file
+    inc fibonacciProgram = "fibonacci.os.js"; //assign the include into a variable if we want to be able to run the whole program
+    inc "fibonacci.os.js"; //ignore the main program, just include the public namespaces from the file
 
     fibonacciProgram.run(); //if included with assign to variable. this command will effectively release control to the called program fully, until that one terminates with end, or with exit from the main block, or with whatever my asynchronous "Application.ProcessEvents" equivalent is. (sleep?)
     //in both cases though, i can use
